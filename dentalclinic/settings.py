2aadfa718cc901c413d0d79d00129d9bf160f9c5
Dheeraj Kumar1 <Dheeraj.Kumar1@jxnoida.com>
2025-05-02 13:36:57 +0530

    Initial commit

--- dentalclinic/settings.py
+++ dentalclinic/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -111,15 +112,10 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']  # for development
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # settings.py
 DEFAULT_FROM_EMAIL = 'dentalcareebenezer@gmail.com'
 CLINIC_EMAIL = 'dentalcareebenezer@gmail.com'  # Replace with your clinic's email
